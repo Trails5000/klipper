47b5a91f96761961e693031fa514a0025a877117
Annex-Engineering <92735457+Trails5000@users.noreply.github.com>
2021-10-18 10:54:55 -0400

    z tilt uses adjustments instead of standard converging process
    converges faster and more consistently with 2 or 4 z motors, equal speed on 3 z motors

--- klippy/extras/z_tilt.py
+++ klippy/extras/z_tilt.py
@@ -171,7 +171,8 @@ class ZTilt:
                        for x, y in self.z_positions]
         self.z_helper.adjust_steppers(adjustments, speed)
         return self.z_status.check_retry_result(
-            self.retry_helper.check_retry([p[2] for p in positions]))
+            #self.retry_helper.check_retry([p[2] for p in positions]))
+            self.retry_helper.check_retry(adjustments) #use adjustments instead, converges faster
     def get_status(self, eventtime):
             return self.z_status.get_status(eventtime)
 
